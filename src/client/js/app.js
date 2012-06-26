1eb0fb512407add2ecbdbb8e05bede343a8e83ba
David Rio Deiros <driodeiros@gmail.com>
2012-06-26 15:52:15 -0500

    * Adding a minimal protocol to send info about the status of   egenotyping via socket.io.

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -5,8 +5,13 @@ function main() {
 
   // Connect via socket.io to communicate with the server
   // in the socket we will get the updates for the heatmap
-  var socket = io.connect('http://localhost:8080');
+  var socket = io.connect('http://localhost:8080'),
+      jdata; // json data
   socket.on('message', function(data){
-    console.log(data);
+    jdata = JSON.parse(data);
+    if (jdata.type === 'info') {
+      d3.select("#info")
+        .text("# reads processed: " + jdata.n_reads + " | # of hits: " + jdata.n_hits);
+    }
   });
 }
