366805e9da00cc007cb091697247ee58f1f3e1d1
David Rio Deiros <driodeiros@gmail.com>
2012-06-26 15:01:22 -0500

    * Listening in socket. Dumping to console if we get any message.

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -3,11 +3,8 @@ function main() {
   heatmap.empty(heatmap.random_data(), {}, "#heatmap");
   heatmap.display_palette();
 
-  /*
   var socket = io.connect('http://localhost:8080');
   socket.on('message', function(data){
-    //d3.select("#log").text(data);
-    $("pre").append(data + "\n");
+    console.log(data);
   });
-  */
 }
