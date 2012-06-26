2bc84044991b0e559f41f46afbae5cbde7238a36
David Rio Deiros <driodeiros@gmail.com>
2012-06-26 15:05:14 -0500

    * Adding more logs.

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -3,6 +3,8 @@ function main() {
   heatmap.empty(heatmap.random_data(), {}, "#heatmap");
   heatmap.display_palette();
 
+  // Connect via socket.io to communicate with the server
+  // in the socket we will get the updates for the heatmap
   var socket = io.connect('http://localhost:8080');
   socket.on('message', function(data){
     console.log(data);
